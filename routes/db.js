591d82ea36166ce468f0878a34d64fe56cc578c8
Stella <stella.widyasari@gmail.com>
2017-04-13 00:53:23 +0800

    [ADD]Post but unable to connect to MongoDB

--- routes/db.js
+++ routes/db.js
@@ -19,17 +19,34 @@ mongoose.Promise = global.Promise;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     // var resp = logData.displayAll();
-    var logData = new Log({
-        log: {}
-    });
-
-    logData.save(function(err) {
-        if (err) throw err;
-        console.log("Tweets saved successfully");
-    })
+    // var logData = new Log({
+    //     log: {}
+    // });
+    //
+    // logData.save(function(err) {
+    //     if (err) throw err;
+    //     console.log("Tweets saved successfully");
+    // })
+    res.send("i am here");
     // console.log("===response====");
     // console.log(resp);
     // res.send(resp);2
 });
+
+router.post('/', function(req, res) {
+    console.log("corresponding");
+    console.log(req.body);
+
+    var logData = new Log({
+        log: req
+    });
+
+    logData.save(function(err) {
+        if (err) throw err;
+        console.log("getting json");
+    })
+
+    res.send('POST request to the homepage')
+})
 
 module.exports = router;
